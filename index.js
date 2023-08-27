4a8d8bb3c2b74e43a54b2b0280b006f7b94c694a
unknown <miss-ndeya>
2023-08-27 20:16:54 +0200

    frist commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnEnvoyer = document.getElementById("btnEnvoyer");
+const mesClasses = document.querySelectorAll(".form-control")
+btnEnvoyer.addEventListener('click', () => {
+    window.local = "index.html"
+    console.log("marche");
+})
